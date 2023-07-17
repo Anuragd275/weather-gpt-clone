1deabc3abba18a44cc89adbf8ec06f80351c2d81
Anurag Dubey <71247280+Anuragd275@users.noreply.github.com>
2023-07-17 23:30:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function getCurrentTemperature() {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+
+      const apiEndpoint = `http://api.weatherapi.com/v1/current.json?key=26e1733cea604488a77134653231301&q=${latitude},${longitude}&aqi=no`;
+
+      fetch(apiEndpoint)
+        .then((response) => response.json())
+        .then((data) => {
+            const currentTemperature = data.current.temp_c;
+            const location = data.location.name;
+
+          // Update the temperature in the HTML element with id 'temperature'
+          document.getElementById(
+            "temperature"
+            ).textContent = `${currentTemperature}°C`;
+            document.getElementById('location').textContent = `${location}`;
+        })
+        .catch((error) => {
+          console.log("Error:", error);
+        });
+    });
+  }
+
+  // Call the function to get the current temperature when the page loads
+  window.addEventListener("load", getCurrentTemperature);
